ae5b7711e941c85b419a284f19705c6c43fe2125
Thorben Dahl <thorben@sjostrom.no>
2016-09-09 16:01:30 +0200

    Reorganize, switch to Flask, still not tested

--- request2liquidsoap.py
+++ request2liquidsoap.py
@@ -8,20 +8,29 @@ import pyotp
 import flask
 import yaml
 import os.path
+import gunicorn.app.base
+
+################################################################################
+# Define utilities
+################################################################################
 
 
-app = flask.Flask(__name__)
-
-
-def parse_config():
-    with open(os.path.join(os.path.dirname(__file__), 'settings.yaml')) as f:
+def parse_config(configfile):
+    with open(configfile) as f:
         doc = yaml.load(f)
     keyfile = doc["keyfile"]
     socketfile = doc["socketfile"]
     port = doc["port"]
-    allowed_hosts = doc["allowed_hosts"]
+    allowed_remote_addresses = doc["allowed_remote_addresses"]
 
-    return keyfile, socketfile, port, allowed_hosts
+    return keyfile, socketfile, port, allowed_remote_addresses
+
+
+def init_otps(keyfile):
+    with open(keyfile, "r") as f:
+        lines = f.readlines()
+    keys = [line.strip() for line in lines if line.strip()]
+    return [pyotp.TOTP(key) for key in keys]
 
 
 class LiquidSoapBoolean:
@@ -62,49 +71,82 @@ class LiquidSoapBoolean:
         self.close()
 
 
-class HTTPRequestHandler(BaseHTTPRequestHandler):
-    allowed_hosts = ["IP-ADDRESSES HERE"]
+################################################################################
+# Initialize globals
+################################################################################
 
-    def do_GET(self):
-        # TODO: Check that it is an allowed host
-        user_key = int(self.path[1:])
-        our_otps = [otp.now() for otp in self.server.otps[:1]]
+# Init the Flask application
+app = flask.Flask('request2liquidsoap')
+# Where to find the settings
+CONFIGFILE = os.path.join(os.path.dirname(__file__), 'settings.yaml')
+# Parse settings
+KEYFILE, SOCKETFILE, PORT, ALLOWED_REMOTE_ADDRESSES = \
+    parse_config(CONFIGFILE)
+# Init the one time password objects
+OTPS = init_otps(KEYFILE)
 
-        # TODO: Protect against side-chaining attacks
-        do_status = (user_key - our_otps[0]) == 0
-        do_activate = (user_key - our_otps[0]) == our_otps[1]
-        do_deactivate = (user_key - our_otps[0]) == -our_otps[1]
 
-        sleep(1)  # Weak protection against side-chaining and brute-force
+################################################################################
+# Define and run the Flask server
+################################################################################
 
-        if not (do_status or do_activate or do_deactivate):
-            # TODO: Send error
+@app.before_request
+def deny_if_not_allowed_remote_address():
+    remote_address = flask.request.remote_addr
+    if remote_address not in ALLOWED_REMOTE_ADDRESSES:
+        flask.abort(401)
+
+
+@app.route("/<user_key>")
+def handle(user_key):
+    user_key = int(user_key)
+    our_otps = [otp.now() for otp in OTPS[:1]]
+
+    # TODO: Protect against side-chaining attacks
+    do_status = (user_key - our_otps[0]) == 0
+    do_activate = (user_key - our_otps[0]) == our_otps[1]
+    do_deactivate = (user_key - our_otps[0]) == -our_otps[1]
+
+    sleep(1)  # Weak protection against side-chaining and brute-force
+
+    if not (do_status or do_activate or do_deactivate):
+        # TODO: Send error
+        pass
+    else:
+        if do_status:
+            # TODO: Fetch value
             pass
-        else:
-            if do_status:
-                # TODO: Fetch value
-                pass
-            elif do_activate:
-                # TODO: Set interactive boolean to True
-                pass
-            elif do_deactivate:
-                # TODO: Set interactive boolean to False
-                pass
-            # TODO: Send response with status==200 and confirmation codes
+        elif do_activate:
+            # TODO: Set interactive boolean to True
+            pass
+        elif do_deactivate:
+            # TODO: Set interactive boolean to False
+            pass
+        # TODO: Send response with status==200 and confirmation codes
 
 
-class CustomHTTPServer(HTTPServer):
-    # TODO: Set self.otps and self.allowed_hosts
-    pass
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
+    def __init__(self, application, options=None):
+        self.options = options or dict()
+        self.application = application
+        super().__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
 
 
 def main():
-    # TODO: Parse command line arguments
-    # TODO: Set up server
-    # TODO: Start serving!
-    pass
+    options = {
+        'bind': '%s:%s' % ('0.0.0.0', str(PORT)),
+        'workers': 1,
+    }
+    StandaloneApplication(app, options).run()
 
-
-
-
-
+if __name__ == '__main__':
+    main()
