b50aedf9c2b2bdf71f24a4b484f1c54efdafbdcd
Thorben Dahl <thorben@sjostrom.no>
2016-09-15 16:39:50 +0200

    Start work on new format for keyfile.txt

--- generate_keyfile.py
+++ generate_keyfile.py
@@ -1,8 +1,13 @@
 import pyotp
 import argparse
+import base64
+import os
 
 parser = argparse.ArgumentParser(
-    description="Generate keyfile.txt"
+    description="Generate keyfile.txt, which contains two One Time "
+                "Password-keys (one for each direction of communication), and "
+                "one shared 'password' for authenticating the slack2request "
+                "program."
 )
 
 parser.add_argument(
@@ -17,5 +22,7 @@ parser.add_argument(
 args = parser.parse_args()
 keyfile = args.keyfile
 
-keyfile.writelines([pyotp.random_base32() + "\n" for _ in range(4)])
+keyfile.writelines(
+    [pyotp.random_base32(), pyotp.random_base32(),
+     base64.b64encode(os.urandom(64))])
 keyfile.close()
